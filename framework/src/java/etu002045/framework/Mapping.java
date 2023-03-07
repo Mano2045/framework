880d3ca6abaf5cb5c4e0645e529b56eca463353f
kodar <kodar@kodar-Lenovo-ideapad-110-15ISK>
2023-03-07 15:12:41 +0300

    sprint2

--- framework/src/java/etu002045/framework/Mapping.java
+++ framework/src/java/etu002045/framework/Mapping.java
@@ -28,5 +28,4 @@ public class Mapping {
         this.method = method;
     }
     
-    
 }
--- framework/src/java/etu002045/framework/servlet/FrontServlet.java
+++ framework/src/java/etu002045/framework/servlet/FrontServlet.java
@@ -7,13 +7,11 @@ package etu002045.framework.servlet;
 
 import etu002045.framework.Mapping;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 /**
  *
