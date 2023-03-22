7b76b97c70eef482119f0d3014eaef2e5773e676
kodar <kodar@kodar-Lenovo-ideapad-110-15ISK>
2023-03-22 17:14:17 +0300

    framework sprint5

---
+++ framework/src/java/etu002045/framework/ModeleView.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu002045.framework;
+
+/**
+ *
+ * @author kodar
+ */
+public class ModeleView {
+    String view;
+    
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+}
---
+++ framework/src/java/etu002045/framework/buildjar.sh
@@ -0,0 +1,2 @@
+javac -d . *.java
+jar cvf test-frame.jar etu002045/framework/*.class
